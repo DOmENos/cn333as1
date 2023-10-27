28445bee21fd5efd66c6b8b91adc9b63469e17fa
DOmENos <domhaha9@gmail.com>
2023-10-27 21:28:06 +0700

    Initial Commit

---
+++ MemorizGame/MemorizGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizGameApp.swift
+//  MemorizGame
+//
+//  Created by Setthanan Thitathanapat on 27/10/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct MemorizGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
